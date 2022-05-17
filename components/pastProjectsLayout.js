885053e2b297b1a7da3858afb898f4512e00c183
root <root@LAPTOP-F0QBAPD4.localdomain>
2022-05-16 23:31:55 -0400

    layout for ongoing and past project pages

--- components/pastProjectsLayout.js
+++ components/pastProjectsLayout.js
@@ -1,9 +1,37 @@
 export default function PastProjectsLayout(){
     return(
     <div class = "min-h-screen flex items-center justify-center">
-        <div class = "font-poppins">
-            hello past projects
-        </div>
+        <div class = "font-poppins mt-32">
+            <div class="text-center text-3xl">Here's what we worked on in the past</div>
+
+            <table class="table-fixed justify-center mx-32 mt-14">
+                <tbody>
+                    <tr class="">
+                        <td class="w-2/5 px-14 pb-14">image</td>
+                        <td class="whitespace-normal w-3/5 px-14 pb-14">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae diam eu lorem iaculis accumsan. Fusce venenatis tortor nec aliquet porta. Suspendisse ultrices tortor justo, eget finibus nunc suscipit vitae. Ut erat nulla, tincidunt nec sapien et, feugiat auctor neque. Cras cursus justo justo, non vestibulum risus viverra nec. Nam vitae justo porttitor, lacinia dui nec, ultricies orci. Etiam sit amet pharetra risus. Phasellus commodo, quam eu facilisis mattis, leo nibh rutrum nibh, vel pretium justo massa a est. Donec quam nisl, tincidunt quis sem quis, tincidunt sodales turpis. Proin ut sodales enim.</td>
+                    </tr>
+
+                    <tr class="">
+                        <td class="w-2/5 px-14 pb-14">image</td>
+                        <td class="whitespace-normal w-3/5 px-14 pb-14">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae diam eu lorem iaculis accumsan. Fusce venenatis tortor nec aliquet porta. Suspendisse ultrices tortor justo, eget finibus nunc suscipit vitae. Ut erat nulla, tincidunt nec sapien et, feugiat auctor neque. Cras cursus justo justo, non vestibulum risus viverra nec. Nam vitae justo porttitor, lacinia dui nec, ultricies orci. Etiam sit amet pharetra risus. Phasellus commodo, quam eu facilisis mattis, leo nibh rutrum nibh, vel pretium justo massa a est. Donec quam nisl, tincidunt quis sem quis, tincidunt sodales turpis. Proin ut sodales enim.</td>
+                    </tr>
+
+                    <tr>
+                        <td class="w-2/5 px-14 pb-14">image</td>
+                        <td class="whitespace-normal w-3/5 px-14 pb-14">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae diam eu lorem iaculis accumsan. Fusce venenatis tortor nec aliquet porta. Suspendisse ultrices tortor justo, eget finibus nunc suscipit vitae. Ut erat nulla, tincidunt nec sapien et, feugiat auctor neque. Cras cursus justo justo, non vestibulum risus viverra nec. Nam vitae justo porttitor, lacinia dui nec, ultricies orci. Etiam sit amet pharetra risus. Phasellus commodo, quam eu facilisis mattis, leo nibh rutrum nibh, vel pretium justo massa a est. Donec quam nisl, tincidunt quis sem quis, tincidunt sodales turpis. Proin ut sodales enim.</td>
+                    </tr>
+
+                    <tr>
+                        <td class="w-2/5 px-14 pb-14">image</td>
+                        <td class="whitespace-normal w-3/5 px-14 pb-14">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae diam eu lorem iaculis accumsan. Fusce venenatis tortor nec aliquet porta. Suspendisse ultrices tortor justo, eget finibus nunc suscipit vitae. Ut erat nulla, tincidunt nec sapien et, feugiat auctor neque. Cras cursus justo justo, non vestibulum risus viverra nec. Nam vitae justo porttitor, lacinia dui nec, ultricies orci. Etiam sit amet pharetra risus. Phasellus commodo, quam eu facilisis mattis, leo nibh rutrum nibh, vel pretium justo massa a est. Donec quam nisl, tincidunt quis sem quis, tincidunt sodales turpis. Proin ut sodales enim.</td>
+                    </tr>
+
+
+                </tbody>
+
+            </table>
+        </div> 
     </div>
+    
     )
 } 
