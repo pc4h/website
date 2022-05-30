a0cb49dacef1f9f3ab4e0e4cacd22324341e1d13
root <root@LAPTOP-F0QBAPD4.localdomain>
2022-05-30 15:18:43 -0400

    alt backgrounds, image formatting, section headers

--- components/pastProjectsLayout.js
+++ components/pastProjectsLayout.js
@@ -1,29 +1,67 @@
 export default function PastProjectsLayout(){
     return(
-    <div class = "min-h-screen flex items-center justify-center">
-        <div class = "font-poppins mt-32">
-            <div class="text-center text-3xl">Here's what we worked on in the past</div>
+        <div class = "min-h-screen flex items-center justify-center">
+        <div class = "font-poppins">
+            <div class="text-center text-3xl bg-[#F0F8FF] pt-48 pb-14">
+                Here's what we've worked on in the past
+            </div>
 
-            <table class="table-fixed justify-center mx-32 mt-14">
+            <table class="table-fixed justify-items-center pr-32">
                 <tbody>
-                    <tr class="">
-                        <td class="w-2/5 px-14 pb-14">image</td>
-                        <td class="whitespace-normal w-3/5 px-14 pb-14">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae diam eu lorem iaculis accumsan. Fusce venenatis tortor nec aliquet porta. Suspendisse ultrices tortor justo, eget finibus nunc suscipit vitae. Ut erat nulla, tincidunt nec sapien et, feugiat auctor neque. Cras cursus justo justo, non vestibulum risus viverra nec. Nam vitae justo porttitor, lacinia dui nec, ultricies orci. Etiam sit amet pharetra risus. Phasellus commodo, quam eu facilisis mattis, leo nibh rutrum nibh, vel pretium justo massa a est. Donec quam nisl, tincidunt quis sem quis, tincidunt sodales turpis. Proin ut sodales enim.</td>
+                    <tr class="bg-[#CAE9F5]">
+                            <td class="w-2/5 pr-14 pl-28 text-center">
+                                <img src="insert_photo.jpg" width="220"></img>
+                            </td>
+
+                            <td class="whitespace-normal w-3/5 px-14 pb-14">
+                                <h3 class="text-center text-xl mb-4 pt-4">
+                                    Project #1
+                                </h3>
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae diam eu lorem iaculis accumsan. Fusce venenatis tortor nec aliquet porta. Suspendisse ultrices tortor justo, eget finibus nunc suscipit vitae. Ut erat nulla, tincidunt nec sapien et, feugiat auctor neque. Cras cursus justo justo, non vestibulum risus viverra nec. Nam vitae justo porttitor, lacinia dui nec, ultricies orci. Etiam sit amet pharetra risus. Phasellus commodo, quam eu facilisis mattis, leo nibh rutrum nibh, vel pretium justo massa a est. Donec quam nisl, tincidunt quis sem quis, tincidunt sodales turpis. Proin ut sodales enim.
+                            </td>
                     </tr>
 
-                    <tr class="">
-                        <td class="w-2/5 px-14 pb-14">image</td>
-                        <td class="whitespace-normal w-3/5 px-14 pb-14">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae diam eu lorem iaculis accumsan. Fusce venenatis tortor nec aliquet porta. Suspendisse ultrices tortor justo, eget finibus nunc suscipit vitae. Ut erat nulla, tincidunt nec sapien et, feugiat auctor neque. Cras cursus justo justo, non vestibulum risus viverra nec. Nam vitae justo porttitor, lacinia dui nec, ultricies orci. Etiam sit amet pharetra risus. Phasellus commodo, quam eu facilisis mattis, leo nibh rutrum nibh, vel pretium justo massa a est. Donec quam nisl, tincidunt quis sem quis, tincidunt sodales turpis. Proin ut sodales enim.</td>
+                    <tr class="bg-[#AFDCEB]">
+                            <td class="w-2/5 pr-14 pl-28 text-center">
+                                <img src="insert_photo.jpg" width="220"></img>
+                            </td>
+
+                            <td class="whitespace-normal w-3/5 px-14 pb-14">
+                                <h3 class="text-center text-xl mb-4 pt-4">
+                                    Project #2
+                                </h3>
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae diam eu lorem iaculis accumsan. Fusce venenatis tortor nec aliquet porta. Suspendisse ultrices tortor justo, eget finibus nunc suscipit vitae. Ut erat nulla, tincidunt nec sapien et, feugiat auctor neque. Cras cursus justo justo, non vestibulum risus viverra nec. Nam vitae justo porttitor, lacinia dui nec, ultricies orci. Etiam sit amet pharetra risus. Phasellus commodo, quam eu facilisis mattis, leo nibh rutrum nibh, vel pretium justo massa a est. Donec quam nisl, tincidunt quis sem quis, tincidunt sodales turpis. Proin ut sodales enim.
+                            </td>
                     </tr>
 
-                    <tr>
-                        <td class="w-2/5 px-14 pb-14">image</td>
-                        <td class="whitespace-normal w-3/5 px-14 pb-14">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae diam eu lorem iaculis accumsan. Fusce venenatis tortor nec aliquet porta. Suspendisse ultrices tortor justo, eget finibus nunc suscipit vitae. Ut erat nulla, tincidunt nec sapien et, feugiat auctor neque. Cras cursus justo justo, non vestibulum risus viverra nec. Nam vitae justo porttitor, lacinia dui nec, ultricies orci. Etiam sit amet pharetra risus. Phasellus commodo, quam eu facilisis mattis, leo nibh rutrum nibh, vel pretium justo massa a est. Donec quam nisl, tincidunt quis sem quis, tincidunt sodales turpis. Proin ut sodales enim.</td>
+                    <tr class="bg-[#CAE9F5]">
+                            <td class="w-2/5 pr-14 pl-28 text-center">
+                                <img src="insert_photo.jpg" width="220"></img>
+                            </td>
+
+                            <td class="whitespace-normal w-3/5 px-14 pb-14">
+                                <h3 class="text-center text-xl mb-4 pt-4">
+                                    Project #3
+                                </h3>
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae diam eu lorem iaculis accumsan. Fusce venenatis tortor nec aliquet porta. Suspendisse ultrices tortor justo, eget finibus nunc suscipit vitae. Ut erat nulla, tincidunt nec sapien et, feugiat auctor neque. Cras cursus justo justo, non vestibulum risus viverra nec. Nam vitae justo porttitor, lacinia dui nec, ultricies orci. Etiam sit amet pharetra risus. Phasellus commodo, quam eu facilisis mattis, leo nibh rutrum nibh, vel pretium justo massa a est. Donec quam nisl, tincidunt quis sem quis, tincidunt sodales turpis. Proin ut sodales enim.
+                            </td>
                     </tr>
 
-                    <tr>
-                        <td class="w-2/5 px-14 pb-14">image</td>
-                        <td class="whitespace-normal w-3/5 px-14 pb-14">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae diam eu lorem iaculis accumsan. Fusce venenatis tortor nec aliquet porta. Suspendisse ultrices tortor justo, eget finibus nunc suscipit vitae. Ut erat nulla, tincidunt nec sapien et, feugiat auctor neque. Cras cursus justo justo, non vestibulum risus viverra nec. Nam vitae justo porttitor, lacinia dui nec, ultricies orci. Etiam sit amet pharetra risus. Phasellus commodo, quam eu facilisis mattis, leo nibh rutrum nibh, vel pretium justo massa a est. Donec quam nisl, tincidunt quis sem quis, tincidunt sodales turpis. Proin ut sodales enim.</td>
+                    <tr class="bg-[#AFDCEB]">
+                            <td class="w-2/5 pr-14 pl-28 text-center">
+                                <img src="insert_photo.jpg" width="220"></img>
+                            </td>
+
+                            <td class="whitespace-normal w-3/5 px-14 pb-14">
+                                <h3 class="text-center text-xl mb-4 pt-4">
+                                    Project #4
+                                </h3>
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae diam eu lorem iaculis accumsan. Fusce venenatis tortor nec aliquet porta. Suspendisse ultrices tortor justo, eget finibus nunc suscipit vitae. Ut erat nulla, tincidunt nec sapien et, feugiat auctor neque. Cras cursus justo justo, non vestibulum risus viverra nec. Nam vitae justo porttitor, lacinia dui nec, ultricies orci. Etiam sit amet pharetra risus. Phasellus commodo, quam eu facilisis mattis, leo nibh rutrum nibh, vel pretium justo massa a est. Donec quam nisl, tincidunt quis sem quis, tincidunt sodales turpis. Proin ut sodales enim.
+                            </td>
                     </tr>
 
 
@@ -32,6 +70,5 @@ export default function PastProjectsLayout(){
             </table>
         </div> 
     </div>
-    
     )
 } 
