bb3c7c8d5590ce772a06a8f775955291cb3a496f
ben <333bxx@gmail.com>
2022-06-04 13:51:55 -0500

    change to uppercase

--- components/contactUsLayout.js
+++ components/contactUsLayout.js
@@ -2,7 +2,7 @@ export default function ContactUsLayout(){
     return(
     <div class = "min-h-screen flex items-center justify-center">
         <div class = "font-poppins">
-            hello contact us
+            HELLO CONTACT US
         </div>
     </div>
     )
