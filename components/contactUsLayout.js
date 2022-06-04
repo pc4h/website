11cd2497cc8867d6452fe4a8a8b41490183f23b2
ben <333bxx@gmail.com>
2022-06-04 17:07:34 -0500

    I added a lot of stuff

--- components/contactUsLayout.js
+++ components/contactUsLayout.js
@@ -1,8 +1,35 @@
 export default function ContactUsLayout(){
     return(
-    <div class = "min-h-screen flex items-center justify-center">
-        <div class = "font-poppins">
-            HELLO CONTACT US
+    <div class = "min-h-screen flex pt-28 justify-center">
+        <div class = "p-20 w-full h-full font-poppins">
+            <contact>
+                <div class = "text-3xl">Contact Us</div>
+                <div class = "pl-8">
+                    <div class = "pt-16 text-2xl">Contact Info blah blah how to reach etc.</div>
+                    <div class = "text-lg">Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                    ut labore et dolore magna aliqua.</div>
+                </div>
+            </contact>
+            <apply>
+                <div class = "pt-20 text-3xl">Apply to become a member!</div>
+                <div class = "pl-8">
+                    <div class = "pt-20 text-2xl">Instructions here</div>
+                    <div class = "text-lg">Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                    ut labore et dolore magna aliqua.</div>
+                    <ol class = "list-decimal pl-16">
+                        <li class="pl-8">Lorem ipsum dolor sit amet, consectetur 
+                        adipiscing elit, sed do eiusmod tempor incididunt 
+                        ut labore et dolore magna aliqua.</li> 
+                        <li class="pl-8">Lorem ipsum dolor sit amet, consectetur 
+                        adipiscing elit, sed do eiusmod tempor incididunt 
+                        ut labore et dolore magna aliqua.</li> 
+                    </ol>
+                    <div class="text-2xl">Link:&nbsp;
+                <a class="underline text-sky-400 hover:text-sky-600" href="https://loremipsum.io">https://loremipsum.io</a></div>
+                </div>
+            </apply>
         </div>
     </div>
     )
