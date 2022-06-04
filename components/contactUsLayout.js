6a43747a13e23bdaaa08f86e730cb71843bd64a2
ben <333bxx@gmail.com>
2022-06-04 17:33:55 -0500

    Bolded some text

--- components/contactUsLayout.js
+++ components/contactUsLayout.js
@@ -3,9 +3,9 @@ export default function ContactUsLayout(){
     <div class = "min-h-screen flex pt-28 justify-center">
         <div class = "p-20 w-full h-full font-poppins">
             <contact>
-                <div class = "text-3xl">Contact Us</div>
+                <div class = "text-3xl font-bold">Contact Us</div>
                 <div class = "pl-8">
-                    <div class = "pt-16 text-2xl">Contact Info blah blah how to reach etc.</div>
+                    <div class = "pt-16 text-2xl font-semibold">Contact Info blah blah how to reach etc.</div>
                     <div class = "text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
                     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
                     ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
@@ -14,9 +14,9 @@ export default function ContactUsLayout(){
                 </div>
             </contact>
             <apply>
-                <div class = "pt-20 text-3xl">Apply to become a member!</div>
+                <div class = "pt-20 text-3xl font-bold">Apply to become a member!</div>
                 <div class = "pl-8">
-                    <div class = "pt-20 text-2xl">Instructions here</div>
+                    <div class = "pt-20 text-2xl font-semibold">Instructions here</div>
                     <div class = "text-xl">Lorem ipsum dolor sit amet, 
                     consectetur adipiscing elit, sed do eiusmod tempor incididunt 
                     ut labore et dolore magna aliqua.</div>
@@ -28,8 +28,8 @@ export default function ContactUsLayout(){
                         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus luctus 
                         accumsan tortor posuere ac ut consequat semper.</li> 
                     </ol>
-                    <div class="text-2xl">Link:&nbsp;
-                <a class="underline text-sky-400 hover:text-sky-600" href="https://loremipsum.io">https://loremipsum.io</a></div>
+                    <div class="text-2xl font-medium">Link:&nbsp;
+                <a class="underline text-sky-400 hover:text-sky-600 font-normal" href="https://loremipsum.io">https://loremipsum.io</a></div>
                 </div>
             </apply>
         </div>
