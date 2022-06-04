08ddde81ea9455eef38e519056f017a748023468
ben <333bxx@gmail.com>
2022-06-04 17:13:03 -0500

    Created Contacts page

--- components/contactUsLayout.js
+++ components/contactUsLayout.js
@@ -6,25 +6,27 @@ export default function ContactUsLayout(){
                 <div class = "text-3xl">Contact Us</div>
                 <div class = "pl-8">
                     <div class = "pt-16 text-2xl">Contact Info blah blah how to reach etc.</div>
-                    <div class = "text-lg">Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                    ut labore et dolore magna aliqua.</div>
+                    <div class = "text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
                 </div>
             </contact>
             <apply>
                 <div class = "pt-20 text-3xl">Apply to become a member!</div>
                 <div class = "pl-8">
                     <div class = "pt-20 text-2xl">Instructions here</div>
-                    <div class = "text-lg">Lorem ipsum dolor sit amet, 
+                    <div class = "text-xl">Lorem ipsum dolor sit amet, 
                     consectetur adipiscing elit, sed do eiusmod tempor incididunt 
                     ut labore et dolore magna aliqua.</div>
                     <ol class = "list-decimal pl-16">
-                        <li class="pl-8">Lorem ipsum dolor sit amet, consectetur 
-                        adipiscing elit, sed do eiusmod tempor incididunt 
-                        ut labore et dolore magna aliqua.</li> 
-                        <li class="pl-8">Lorem ipsum dolor sit amet, consectetur 
-                        adipiscing elit, sed do eiusmod tempor incididunt 
-                        ut labore et dolore magna aliqua.</li> 
+                        <li class="pl-8 text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus luctus 
+                        accumsan tortor posuere ac ut consequat semper.</li> 
+                        <li class="pl-8 text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus luctus 
+                        accumsan tortor posuere ac ut consequat semper.</li> 
                     </ol>
                     <div class="text-2xl">Link:&nbsp;
                 <a class="underline text-sky-400 hover:text-sky-600" href="https://loremipsum.io">https://loremipsum.io</a></div>
