8708137d869c36f44170ddb6f454e698bfc7fb37
ben <333bxx@gmail.com>
2022-06-04 17:57:46 -0500

    Added color

--- components/contactUsLayout.js
+++ components/contactUsLayout.js
@@ -1,12 +1,12 @@
 export default function ContactUsLayout(){
     return(
-    <div class = "min-h-screen flex pt-28 justify-center">
-        <div class = "p-20 w-full h-full font-poppins">
+    <div class = "min-h-screen flex justify-between">
+        <div class = "px-24 pt-24 w-full h-full font-poppins">
             <contact>
-                <div class = "text-3xl font-bold">Contact Us</div>
-                <div class = "pl-8">
-                    <div class = "pt-16 text-2xl font-semibold">Contact Info blah blah how to reach etc.</div>
-                    <div class = "text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                <div class = "mt-20 text-3xl font-bold w-fit p-4 bg-green-400 h-fit rounded-3xl bg-opacity-50">Contact Us</div>
+                <div class = "ml-8">
+                    <div class = "mt-8 p-4 text-2xl font-semibold">Contact Info blah blah how to reach etc.</div>
+                    <div class = "p-4 text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
                     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
                     ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
                     voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
@@ -14,17 +14,17 @@ export default function ContactUsLayout(){
                 </div>
             </contact>
             <apply>
-                <div class = "pt-20 text-3xl font-bold">Apply to become a member!</div>
-                <div class = "pl-8">
-                    <div class = "pt-20 text-2xl font-semibold">Instructions here</div>
+                <div class = "mt-20 text-3xl font-bold w-fit p-4 bg-green-400 h-fit rounded-3xl bg-opacity-50">Apply to become a member!</div>
+                <div class = "ml-8">
+                    <div class = "mt-16 text-2xl font-semibold">Instructions here</div>
                     <div class = "text-xl">Lorem ipsum dolor sit amet, 
                     consectetur adipiscing elit, sed do eiusmod tempor incididunt 
                     ut labore et dolore magna aliqua.</div>
-                    <ol class = "list-decimal pl-16">
-                        <li class="pl-8 text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    <ol class = "list-decimal ml-16">
+                        <li class="ml-8 text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus luctus 
                         accumsan tortor posuere ac ut consequat semper.</li> 
-                        <li class="pl-8 text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        <li class="ml-8 text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus luctus 
                         accumsan tortor posuere ac ut consequat semper.</li> 
                     </ol>
